beb1fb1cff2be132efcfc642afafae48c7834d86
therealnoof <hd1912@gmail.com>
2020-10-19 23:09:22 -0400

    Add files via upload
    This is a python script for calling an external risk engine via an api call. The payload is stored and then some conditional logic is made.  If a match occurs a REST call is made to the BIG-IP to terminate all APM sessions.

---
+++ BIG-IP/RiskEngineCall.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+### F5 Networks: Author: Arnulfo Hernandez - Solutions Engineer
+### This is a script demostrating an API call to a risk engine
+### and then parsing the payload and looking for a base level security match
+### if theres a match, the script logs into the BIG-IP and kills all APM sessions
+### This could supplement IAP on APM when dealing with VDI
+### VDI does not fire the HTTP Connector so we cant utilize the per-request flow protections
+
+
+import requests
+import json
+from f5.bigip import ManagementRoot
+
+### Run a call to our Risk Engine API server and place the payload into a variable
+response = requests.get("http://160.1.31.151:5000/api/people")
+
+### Parse the variable JSON payload and pull the Hanscom base level status
+### Place base level status in variable deltaLevel
+apiresults = response.json()
+deltaLevel = apiresults[0]["fname"]
+lockDown = "Delta"
+
+### Logic to determine if base level status "Delta" is a match
+### if a match occurs then send a call to APM to terminate all user sessions
+print("The current FPCON level at Hanscom is: " + deltaLevel)
+
+if deltaLevel == lockDown:
+    mgmt = ManagementRoot("52.61.149.60",'admin','F5Twister!')
+    x = mgmt.tm.util.bash.exec_cmd('run', utilCmdArgs='-c "/usr/bin/sessiondump --delete all"')
+    print("FPCON level is Delta, terminating all sessions.")
+    print(x.commandResult)
+    
+
+
